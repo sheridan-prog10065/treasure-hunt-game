fd5d9526cc11627e83ee290ffea8b90e3772afd7
Prof. Magdin Stoica <magdin.stoica@sheridancollege.ca>
2026-01-16 10:06:22 -0500

    Remove deprecated code for loading the MainPage and replace with modern window and appshell creation.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,11 +1,15 @@
-﻿namespace TreasureHuntGame;
+﻿
+namespace TreasureHuntGame;
 
 public partial class App : Application
 {
     public App()
     {
         InitializeComponent();
-
-        MainPage = new AppShell();
     }
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
 }
